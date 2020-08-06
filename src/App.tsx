debcb381e3224cdda8563da7889a9a46cb3df049
hashimoto <tomorow326@gmail.com>
2020-08-06 23:27:03 +0900

    cardコンポーネント追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
 import InputWithSuggestion from 'components/InputWithSuggestion';
+import BrdCard from 'components/BrdCard';
 
 function App() {
   return (
     <div className="App">
+      <BrdCard text={'にゃんぼ！'} color={'white'} position={{x: 100, y: 400, z: 1}} />
       <InputWithSuggestion />
     </div>
   );
---
+++ src/components/BrdCard/index.tsx
@@ -0,0 +1,64 @@
+import React, { useState, useMemo, useCallback } from 'react';
+import cn from 'classnames/bind';
+import styles from './style.module.scss';
+
+const cx = cn.bind(styles);
+
+interface BrdCardProps {
+  text: string,
+  color: string,
+  position: {
+    x: number,
+    y: number,
+    z: number
+  },
+  handleChange?: (text: string) => unknown,
+  handleClick?: (...args: unknown[]) => unknown,
+  handleDoubleClick?: (...args: unknown[]) => unknown,
+  handleRightClick?: (...args: unknown[]) => unknown,
+}
+
+const BrdCard: React.FC<BrdCardProps> = ({ 
+  text, 
+  color,
+  position,
+  handleChange = () => {},
+  handleRightClick = () => {} 
+}) => {
+  const [isFocus, setIsFocus] = useState<boolean>(false);
+  const classname = cx('card');
+  const style = useMemo(() => {
+    return {
+      backgroundColor: color,
+      top: position.y + 'px',
+      left: position.x + 'px',
+      zIndex: position.z
+    }
+  },[color, position]);
+
+  const handleDoubleClick = useCallback(() => {
+    setIsFocus(true);
+  }, [setIsFocus]);
+
+  const handleBlur = useCallback(() => {
+    setIsFocus(false);
+  }, [setIsFocus]);
+
+  return (
+    <div className={classname} style={style} onDoubleClick={handleDoubleClick} >
+      {
+        isFocus ? 
+          <textarea 
+            className={cx('textarea')} 
+            defaultValue={text} 
+            autoFocus
+            onChange={e => {handleChange(e.target.value)}}
+            onBlur={handleBlur} /> 
+          :
+          <div className={cx('inner')}>{ text }</div>
+      }
+    </div>
+  )
+}
+
+export default BrdCard
