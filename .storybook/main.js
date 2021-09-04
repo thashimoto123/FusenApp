dd637d06a99221d0ae43df759a21d6764cd4beee
thashimoto <thahsimoto@.me>
2021-09-04 15:19:09 +0900

    update Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js','../src/**/*.stories.ts'],
+  stories: ['../src/**/*.stories.js','../src/**/*.stories.ts', '../src/**/*.stories.tsx'],
   addons: [
+    '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
