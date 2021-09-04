28577dc1b7e8c55461485dfa6eb5e236aafba3aa
thashimoto <thahsimoto@.me>
2021-09-04 16:46:42 +0900

    Fix InputWithButton stories

---
+++ src/stories/InputWithButton.stories.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { Meta, Story } from '@storybook/react';
+import InputWithButton, { InputWithButtonProps} from 'components/InputWithButton';
+import 'index.css';
+
+export default {
+  title: 'InputWithButton',
+  component: InputWithButton,
+  argTypes: {
+    value: { control: { type: 'text' } },
+    buttonValue: { control: { type: 'text' } },
+    placeholder: { control: { type: 'text' } },
+  }
+} as Meta;
+
+const Template: Story<InputWithButtonProps> = (args) => <InputWithButton {...args} />
+
+export const Basic = Template.bind({});
+Basic.args = {
+  handleSubmit: (e, ...args) => { e.preventDefault(); action('onSubmit')(e, ...args) },
+  handleChange: action('onChange')
+}
+
+export const WithValue = Template.bind({});
+WithValue.args = {
+  ...Basic.args,
+  value: 'テキスト'
+}
+
+export const CustomButtonValue = Template.bind({});
+CustomButtonValue.args = {
+  ...Basic.args,
+  buttonValue: 'カードを追加'
+}
+
+export const WithPlaceholder = Template.bind({});
+WithPlaceholder.args = {
+  ...Basic.args,
+  placeholder: 'テキストを入力'
+};
